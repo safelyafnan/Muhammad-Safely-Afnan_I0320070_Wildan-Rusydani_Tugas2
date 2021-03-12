fae2007c0dfa6657c2384f7603dc1c6275a63f14
Safely Afnan <safafnan8@gmail.com>
2021-03-12 10:14:07 +0700

    update tugas 2.1

---
+++ I0320070_exercise2.2.py
@@ -0,0 +1,18 @@
+#tipe data boolean
+print(True)
+
+#tipe data string
+print("Ayo belajar python")
+print('Belajar Python Sangat Mudah')
+
+#tipe data Integer
+print(20)
+
+#tipe data Float
+print(3.14)
+
+#tipe data Hexadecimal
+print(9)
+
+#tipe data Complex
+print(5j)
