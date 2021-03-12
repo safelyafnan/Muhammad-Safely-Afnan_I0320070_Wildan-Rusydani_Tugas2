182696d1a0d5c9ce4624e8930647118f2f32a7db
Safely Afnan <safafnan8@gmail.com>
2021-03-12 14:45:04 +0700

    update tugas

---
+++ I0320070_soal1_tugas2.py
@@ -0,0 +1,45 @@
+from math import pi
+
+print("Persegi panjang")
+
+p=float(input("Masukkan panjangnya : "))
+l=float(input("masukan lebarnya : "))
+
+rumus=p*l
+
+print("Luas persegi panjang tersebut adalah ", rumus )
+
+print("lingkaran")
+
+r=float(input("Masukan jari-jari : "))
+
+Rumus=pi*r*r
+
+print("Luas lingkaran tersebut adalah ", Rumus )
+
+print("kubus")
+
+s=float(input("masukan panjang sisi kubus : "))
+
+RUmus=s*s*6
+
+print("Luas kubus adalah ", RUmus )
+
+print("Konversi suhu celcius ke fahrenheit")
+
+c=float(input("Suhu dalam celcius : "))
+
+cf=9/5*c+32
+
+print("suhu {} celcius = {} fahrenheit" .format(c,cf))
+
+print("konversi suhu reamur ke kelvin")
+
+r=float(input("suhu dalam reamur : "))
+
+rk=5/4*r+273.15
+
+print("suhu {} Reamur = {} Kelvin" .format(r,rk))
+
+
+
